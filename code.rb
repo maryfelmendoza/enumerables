1a142fff9ea60e1ec6d88e459e68e2ad1f8df0aa
Maryfel Mendoza <maryfelmendoza@hotmail.com>
2019-10-05 14:42:59 -0500

    Update

--- code.rb
+++ code.rb
@@ -13,44 +13,44 @@ module Enumerable
 
   def my_select
     result = []
-    self.my_each { |i| result.push(i) if yield(i)}
+    self.my_each { |i| result.push(i) if yield(i) }
     result
   end
 
   def my_all?
-    self.my_each { |i| return false unless yield(i)}
+    self.my_each { |i| return false unless yield(i) }
     true
   end
 
   def my_any?
-    self.my_each { |i| return true if yield(i)} 
+    self.my_each { |i| return true if yield(i) } 
     false
   end
 
   def my_none?
-    self.my_each { |i| return false if yield(i)} 
+    self.my_each { |i| return false if yield(i) } 
     true
   end
 
   def my_count
     total = 0
-    self.my_each { |i| if (yield(i)) then total += 1 end}
+    self.my_each { |i| if (yield(i)) then total += 1 end }
     return total
   end
 
   def my_map(&block)
     arr = []
-    self.my_each { |i| result.push(block.call(i))}
+    self.my_each { |i| result.push(block.call(i)) }
     return arr
   end
 
   def my_inject
     memo = self[0]
-    self.my_each { |i| memo = yield(memo, i)}
+    self.my_each { |i| memo = yield(memo, i) }
     memo
   end
 
   def multiply_els
-    arr.my_inject { |x,y| x*y}
+    arr.my_inject { |x, y| x*y }
   end
 end
