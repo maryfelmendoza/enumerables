89eb17dd8f2896405c3ab0138c5b521a63203450
Maryfel Mendoza <maryfelmendoza@hotmail.com>
2019-10-07 19:59:39 -0500

    Update code.rb

--- code.rb
+++ code.rb
@@ -2,13 +2,13 @@
 
 module Enumerable
   def my_each
-    for i in 0...self.length
+    while i < self.length
       yield(self[i])
     end
   end
 
   def my_each_with_index
-    for i in 0...self.length
+    while i < self.length
       yield(self[i], i)
     end
   end
