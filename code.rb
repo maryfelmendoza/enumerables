7a93b72535890974926814a6fbc99df8997a1b2e
Maryfel Mendoza <maryfelmendoza@hotmail.com>
2019-10-11 15:53:09 -0500

    indentation spaces

--- code.rb
+++ code.rb
@@ -2,21 +2,21 @@
 
 module Enumerable
   def my_each
-      i = 0
-      while i < self.size
-        yield(self[i])
-        i += 1
-      end
-      self
+    i = 0
+    while i < self.size
+      yield(self[i])
+      i += 1
+    end
+    self
   end
 
   def my_each_with_index
-      i = 0
-      while i < self.size
-        yield(self[i])
-        i += 1
-      end
-      self
+    i = 0
+    while i < self.size
+      yield(self[i])
+      i += 1
+    end
+    self
   end
 
   def my_select
