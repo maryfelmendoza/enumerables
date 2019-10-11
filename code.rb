da2fe94b002d5228ae89df050a2e1f84ffe18eb2
Maryfel Mendoza <maryfelmendoza@hotmail.com>
2019-10-11 16:10:14 -0500

    self fixed

--- code.rb
+++ code.rb
@@ -21,40 +21,40 @@ module Enumerable
 
   def my_select
     result = []
-    self.my_each { |i| result.push(i) if yield(i) }
+    my_each { |i| result.push(i) if yield(i) }
     result
   end
 
   def my_all?
-    self.my_each { |i| return false unless yield(i) }
+    my_each { |i| return false unless yield(i) }
     true
   end
 
   def my_any?
-    self.my_each { |i| return true if yield(i) }
+    my_each { |i| return true if yield(i) }
     false
   end
 
   def my_none?
-    self.my_each { |i| return false if yield(i) }
+    my_each { |i| return false if yield(i) }
     true
   end
 
   def my_count
     total = 0
-    self.my_each { |i| if yield i then total += 1 end }
+    my_each { |i| if yield i then total += 1 end }
     total
   end
 
   def my_map(&block)
     arr = []
-    self.my_each { |i| result.push(block.call(i)) }
+    my_each { |i| result.push(block.call(i)) }
     arr
   end
 
   def my_inject
     memo = self[0]
-    self.my_each { |i| memo = yield(memo, i) }
+    my_each { |i| memo = yield(memo, i) }
     memo
   end
 
